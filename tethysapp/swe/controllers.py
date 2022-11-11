966a99bef418f9a9c4139f65d4e0bd3befa71f5d
Karnesh Jain <jain.karnesh@gmail.com>
2022-11-11 12:27:10 -0500

    added log file instead of print statements

--- tethysapp/swe/controllers.py
+++ tethysapp/swe/controllers.py
@@ -4,6 +4,9 @@ from tethys_sdk.gizmos import SelectInput
 from .app import Swe as app
 from .thredds_methods import parse_datasets, get_layers_for_wms
 from django.http import HttpResponseNotAllowed, JsonResponse
+import logging
+
+log = logging.getLogger(__name__)
 
 
 @login_required()
@@ -14,12 +17,11 @@ def home(request):
     catalog = app.get_spatial_dataset_service(app.THREDDS_SERVICE_NAME, as_engine=True)
 
     # Retrieve dataset options from the THREDDS service
-    print("Retrieving Datasets...")
+    log.info('Retrieving Datasets...')
     datasets = parse_datasets(catalog)
     initial_dataset_option = datasets[0]
-    from pprint import pprint
-    pprint(datasets)
-    pprint(initial_dataset_option)
+    log.debug(datasets)
+    log.debug(initial_dataset_option)
 
     dataset_select = SelectInput(
         display_text='Dataset',
@@ -69,7 +71,7 @@ def get_wms_layers(request):
     try:
         wms_url = request.GET.get('wms_url', None)
 
-        print(f'Retrieving layers for: {wms_url}')
+        log.info(f'Retrieving layers for: {wms_url}')
         layers = get_layers_for_wms(wms_url)
 
         json_response.update({
